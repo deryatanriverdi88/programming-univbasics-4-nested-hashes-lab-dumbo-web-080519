3bae25aa1d0dcd08eac2eab95880ebbf82550056
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-07-30 23:26:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:language] = "Assembly"
+     programmer_hash[:dennis_ritchie][:language][1] = "Assembly"
      return programmer_hash
 
 end
